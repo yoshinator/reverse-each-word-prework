af9a47f68f8ec387d5605243b2ba64dd5021649e
yoshinator <yoanante@gmail.com>
2018-09-20 15:54:09 -0400

    Changed method after viewing result to condense code.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(string)
-  new_array = string.split
-  new_string = ""
-  new_array.collect do |word|
-    word.reverse!
-  end
-  new_array.join(" ")
+  string.split.collect {|word| word.reverse!}.join(" ")
 end
